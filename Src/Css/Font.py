c42533f63ba3803e21e6191bf01b66031c451544
yanqixiao <yanqixiao@163.com>
2022-11-06 09:48:50 +0800

    chenqi-home

--- Src/Css/Font.py
+++ Src/Css/Font.py
@@ -15,40 +15,54 @@
 import sys
 sys.path.append( "" if hasattr(sys, "_MEIPASS") else __file__[0:__file__.index("Src")] )
 
-from styleSheets import *
+from Src.Css.Base import *
 
 
 class Font(Style):
-    '''
-    # 可设置的属性是（按顺序）: "font-style font-variant font-weight font-size/line-height font-family"
-    # font-size和font-family的值是必需的。如果缺少了其他值, 默认值将被插入, 如果有默认值的话。
-    '''
-    font:str = ""
-    '''
-    # 指定文本的字体系列
-    '''
-    font_family:str = ""
-    '''
-    # medium: 默认值/xx-small,x-small,small,large,x-large,xx-large
-    # smaller/larger
-    # length: 把 font-size 设置为一个固定的值。
-    # %: 把 font-size 设置为基于父元素的一个百分比值。
-    '''
-    font_size:str = ""
-    '''
-    # normal: 默认值。浏览器显示一个标准的字体样式。
-    # italic: 浏览器会显示一个斜体的字体样式。
-    # oblique: 浏览器会显示一个倾斜的字体样式。
-    # inherit: 规定应该从父元素继承字体样式。
-    '''
-    font_style:str = ""
-    '''
-    # normal: 默认值。浏览器会显示一个标准的字体。
-    # small-caps: 浏览器会显示小型大写字母的字体。
-    '''
-    font_variant:str = ""
-    '''
-    # normal: 默认值。定义标准的字符。bold/bolder/lighter
-    # 100~900: 定义由细到粗的字符。400 等同于 normal，而 700 等同于 bold。
-    '''
-    font_weight:str = ""
+    def __init__(self) -> None:
+        super().__init__()
+        self.font:str = ""
+        '''
+        描述: 可设置的属性是（按顺序）: "font-style font-variant font-weight font-size/line-height font-family" \n
+        例如: font-size和font-family的值是必需的。如果缺少了其他值, 默认值将被插入, 如果有默认值的话。
+        '''
+        self.font_family:str = ""
+        '''
+        描述: 指定文本的字体系列 \n
+        例如: p { font-family:"Times New Roman",Georgia,Serif; }
+        '''
+        self.font_size:str = ""
+        '''
+        描述: 指定文本的字体大小 \n
+            *medium: 默认值/xx-small,x-small,small,large,x-large,xx-large \n
+            *smaller/larger \n
+            *length: 把 font-size 设置为一个固定的值。\n
+            *%: 把 font-size 设置为基于父元素的一个百分比值。\n
+        例如: h1 { font-size:250% } \n
+             h2 { font-size: larger; } \n
+             h3 { font-size: xx-large; } \n
+             h4 { font-size: 12px; }
+        '''
+        self.font_style:str = ""
+        '''
+        描述: 指定文本的字体样式 \n
+            *normal: 默认值。浏览器显示一个标准的字体样式。\n
+            *italic: 浏览器会显示一个斜体的字体样式。\n
+            *oblique: 浏览器会显示一个倾斜的字体样式。\n
+            *inherit: 规定应该从父元素继承字体样式。\n
+        例如: p.italic {font-style:italic}
+        '''
+        self.font_variant:str = ""
+        '''
+        描述: 以小型大写字体或者正常字体显示文本。\n
+            *normal: 默认值。浏览器会显示一个标准的字体。\n
+            *small-caps: 浏览器会显示小型大写字母的字体。\n
+        例如: p.small { font-variant:small-caps; }
+        '''
+        self.font_weight:str = ""    
+        '''
+        描述: 指定字体的粗细。\n
+            *normal: 默认值。定义标准的字符。bold/bolder/lighter \n
+            *100~900: 定义由细到粗的字符。400 等同于 normal,而 700 等同于 bold。\n
+        例如: p.thicker { font-weight:900; }
+        '''
--- Src/Css/Text.py
+++ Src/Css/Text.py
@@ -19,110 +19,134 @@ from Src.Css.Base import *
 
 
 class Text(Style):
-    color:str = "" # 设置文本颜色
-    '''
-    # 设置文本方向
-    # ltr: 默认。文本方向从左到右
-    # rtl: 文本方向从右到左。
-    # inherit: 规定应该从父元素继承 direction 属性的值。
-    '''
-    direction:str = "" 
-    '''
-    # 设置字符间距
-    # normal: 默认。规定字符间没有额外的空间。
-    # length: 定义字符间的固定空间（允许使用负值）例如: 2px, 3px
-    # inherit: 规定应该从父元素继承 letter-spacing 属性的值。
-    '''
-    letter_spacing:str = ""
-    '''
-    # 设置行高
-    # normal: 默认。设置合理的行间距。
-    # number: 设置数字，此数字会与当前的字体尺寸相乘来设置行间距。
-    # length: 设置固定的行间距。
-    # %: 基于当前字体尺寸的百分比行间距。
-    # inherit: 规定应该从父元素继承 line-height 属性的值。
-    '''
-    line_height:str = ""
-    '''
-    # 对齐元素中的文本
-    # left: 把文本排列到左边。默认值：由浏览器决定。
-    # right: 把文本排列到右边。
-    # center: 把文本排列到中间。
-    # justify: 实现两端对齐文本效果。
-    # inherit: 规定应该从父元素继承 text-align 属性的值。
-    '''
-    text_align:str = ""
-    '''
-    # 向文本添加修饰
-    # none: 默认。定义标准的文本。
-    # underline: 定义文本下的一条线。
-    # overline: 定义文本上的一条线。
-    # line-through: 定义穿过文本下的一条线。
-    # blink: 定义闪烁的文本。
-    # inherit: 规定应该从父元素继承 text-decoration 属性的值。
-    '''
-    text_decoration:str = ""
-    '''
-    # 缩进元素中文本的首行
-    # length: 定义固定的缩进。默认值: 0。
-    # %: 定义基于父元素宽度的百分比的缩进。
-    # inherit: 规定应该从父元素继承 text-indent 属性的值
-    '''
-    text_indent:str = ""
-    '''
-    # 设置文本阴影
-    # h-shadow: 必需。水平阴影的位置。允许负值。
-    # v-shadow: 必需。垂直阴影的位置。允许负值。
-    # blur: 可选。模糊的距离。
-    # color: 可选。阴影的颜色。参阅 CSS 颜色值。
-    '''
-    text_shadow:str = ""
-    '''
-    # 控制元素中的字母
-    # none: 默认。定义带有小写字母和大写字母的标准的文本。
-    # capitalize: 文本中的每个单词以大写字母开头。
-    # uppercase: 定义仅有大写字母。
-    # lowercase: 定义无大写字母，仅有小写字母。
-    # inherit: 规定应该从父元素继承 text-transform 属性的值。
-    '''
-    text_transform:str = ""
-    '''
-    # 设置或返回文本是否被重写 
-    # normal: 默认。不使用附加的嵌入层面。
-    # embed: 创建一个附加的嵌入层面。
-    # bidi-override: 创建一个附加的嵌入层面。重新排序取决于 direction 属性。
-    # initial: 设置该属性为它的默认值。
-    # inherit: 从父元素继承该属性。
-    '''
-    unicode_bidi:str = ""
-    '''
-    # 设置元素的垂直对齐
-    # baseline: 默认。元素放置在父元素的基线上。
-    # super: 垂直对齐文本的上标
-    # top: 把元素的顶端与行中最高元素的顶端对齐。
-    # text-top: 把元素的顶端与父元素字体的顶端对齐
-    # middle: 使元素及其后代元素的底部与整行的底部对齐。
-    # bottom: 把此元素放置在父元素的中部。
-    # text-bottom: 把元素的底端与父元素字体的底端对齐。
-    # length: 将元素升高或降低指定的高度，可以是负数。
-    # %: 使用 "line-height" 属性的百分比值来排列此元素。允许使用负值。
-    # inherit: 从父元素继承该属性。
-    '''
-    vertical_align:str = ""
-    '''
-    # 设置元素中空白的处理方式
-    # normal: 默认。空白会被浏览器忽略。
-    # pre: 空白会被浏览器保留。其行为方式类似 HTML 中的 <pre> 标签。
-    # nowrap: 文本不会换行，文本会在在同一行上继续，直到遇到 <br> 标签为止。
-    # pre-wrap: 保留空白符序列，但是正常地进行换行。
-    # pre-line: 合并空白符序列，但是保留换行符。
-    # inherit: 从父元素继承该属性。
-    '''
-    white_space:str = ""
-    '''
-    # 设置字间距
-    # normal: 默认。定义单词间的标准空间。
-    # length: 定义单词间的固定空间。
-    # inherit: 从父元素继承该属性。
-    '''
-    word_spacing:str = ""
+    def __init__(self) -> None:
+        super().__init__()
+        self.color:str = "" 
+        '''
+        描述: 设置文本颜色 \n
+        例如: body {color:red;}
+        '''
+        self.direction:str = "" 
+        '''
+        描述: 设置文本方向 \n
+            *ltr: 默认。文本方向从左到右 \n
+            *rtl: 文本方向从右到左。\n
+            *inherit: 规定应该从父元素继承 direction 属性的值。\n
+        例如: div {
+                direction:rtl;
+                unicode-bidi: bidi-override; 
+            }
+        '''
+        self.letter_spacing:str = ""
+        '''
+        描述: 设置字符间距 \n
+            *normal: 默认。规定字符间没有额外的空间。\n
+            *length: 定义字符间的固定空间（允许使用负值）例如: 2px, 3px \n
+            *inherit: 规定应该从父元素继承 letter-spacing 属性的值。\n
+        例如: h1 { letter-spacing:2px }
+        '''
+        self.line_height:str = ""
+        '''
+        描述: 设置行高 \n
+            *normal: 默认。设置合理的行间距。\n
+            *number: 设置数字,此数字会与当前的字体尺寸相乘来设置行间距。\n
+            *length: 设置固定的行间距。\n
+            *%: 基于当前字体尺寸的百分比行间距。\n
+            *inherit: 规定应该从父元素继承 line-height 属性的值。\n
+        例如: p.big { line-height:200% }
+        '''
+        self.text_align:str = ""
+        '''
+        描述: 对齐元素中的文本 \n
+            *left: 把文本排列到左边。默认值：由浏览器决定。\n
+            *right: 把文本排列到右边。\n
+            *center: 把文本排列到中间。\n
+            *justify: 实现两端对齐文本效果。\n
+            *inherit: 规定应该从父元素继承 text-align 属性的值。\n
+        例如: h1 { text-align:center }
+        '''
+        self.text_decoration:str = ""
+        '''
+        描述: 向文本添加修饰 \n
+            *none: 默认。定义标准的文本。\n
+            *underline: 定义文本下的一条线。\n
+            *overline: 定义文本上的一条线。 \n
+            *line-through: 定义穿过文本下的一条线。\n
+            *blink: 定义闪烁的文本。\n
+            *inherit: 规定应该从父元素继承 text-decoration 属性的值。\n
+        例如: h2 { text-decoration:line-through }
+        '''
+        self.text_indent:str = ""
+        '''
+        描述: 缩进元素中文本的首行 \n
+            *length: 定义固定的缩进。默认值: 0。\n
+            *%: 定义基于父元素宽度的百分比的缩进。\n
+            *inherit: 规定应该从父元素继承 text-indent 属性的值。\n
+        例如: p { text-indent:50px; }
+        '''
+        self.text_shadow:str = ""
+        '''
+        描述: 设置文本阴影 \n
+            *h-shadow: 必需。水平阴影的位置。允许负值。\n
+            *v-shadow: 必需。垂直阴影的位置。允许负值。\n
+            *blur: 可选。模糊的距离。\n
+            *color: 可选。阴影的颜色。参阅 CSS 颜色值。\n
+        例如: h1 { text-shadow: 2px 2px #ff0000; }
+        '''
+        self.text_transform:str = ""
+        '''
+        描述: 控制元素中的字母 \n
+            *none: 默认。定义带有小写字母和大写字母的标准的文本。\n
+            *capitalize: 文本中的每个单词以大写字母开头。\n
+            *uppercase: 定义仅有大写字母。\n
+            *lowercase: 定义无大写字母,仅有小写字母。\n
+            *inherit: 规定应该从父元素继承 text-transform 属性的值。\n
+        例如: h2 { text-transform:capitalize; }
+        '''
+        self.unicode_bidi:str = ""
+        '''
+        描述: 设置或返回文本是否被重写 \n
+            *normal: 默认。不使用附加的嵌入层面。\n
+            *embed: 创建一个附加的嵌入层面。\n
+            *bidi-override: 创建一个附加的嵌入层面。重新排序取决于 direction 属性。\n
+            *initial: 设置该属性为它的默认值。\n
+            *inherit: 从父元素继承该属性。\n
+        例如: div {
+                direction:rtl;
+                unicode-bidi:bidi-override;
+            }
+        '''
+        self.vertical_align:str = ""
+        '''
+        描述: 设置元素的垂直对齐 \n
+            *baseline: 默认。元素放置在父元素的基线上。\n
+            *super: 垂直对齐文本的上标. \n
+            *top: 把元素的顶端与行中最高元素的顶端对齐。\n
+            *text-top: 把元素的顶端与父元素字体的顶端对齐. \n
+            *middle: 使元素及其后代元素的底部与整行的底部对齐。 \n
+            *bottom: 把此元素放置在父元素的中部。 \n
+            *text-bottom: 把元素的底端与父元素字体的底端对齐。 \n
+            *length: 将元素升高或降低指定的高度,可以是负数。 \n
+            *%: 使用 "line-height" 属性的百分比值来排列此元素。允许使用负值。 \n
+            *inherit: 从父元素继承该属性。 \n
+        例如: img { vertical-align:text-top; }
+        '''
+        self.white_space:str = ""
+        '''
+        描述: 设置元素中空白的处理方式 \n
+            *normal: 默认。空白会被浏览器忽略。\n
+            *pre: 空白会被浏览器保留。其行为方式类似 HTML 中的 <pre> 标签。\n
+            *nowrap: 文本不会换行,文本会在在同一行上继续,直到遇到 <br> 标签为止。\n
+            *pre-wrap: 保留空白符序列,但是正常地进行换行。\n
+            *pre-line: 合并空白符序列,但是保留换行符。\n
+            *inherit: 从父元素继承该属性。\n
+        例如: p { white-space:nowrap; }
+        '''
+        self.word_spacing:str = ""
+        '''
+        描述: 设置字间距 \n
+            *normal: 默认。定义单词间的标准空间。\n
+            *length: 定义单词间的固定空间。\n
+            *inherit: 从父元素继承该属性。\n
+        例如: p { word-spacing:30px; }
+        '''
